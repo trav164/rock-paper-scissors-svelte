14cee5fbc75d68e8820a78357cbbee9470a99933
trav164 <travis164@gmail.com>
2023-01-17 19:36:19 +0000

    i suck at css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
-<h1 class="text-3xl font-bold underline">Rock Paper Scissors Game with Svelte and Supabase</h1>
+<div class="flex h-screen">
+	<div class="m-auto bg-slate-200 shadow-lg rounded-lg h-64 p-6">
+		<h1 class="text-3xl font-bold underline">Rock Paper Scissors Game with Svelte and Supabase</h1>
 
-<p>Lets play some rock paper scissors!</p>
-<div class="mx-auto text-center bg-slate-400">
-	<button
-		class="p-4 font-medium bg-gradient-to-r from-[#0BAB64] to-[#3BB78F] text-slate-200 shadow-lg rounded-lg"
-		><a href="/login">Login</a></button
-	>
-	<button
-		class="p-4 font-medium bg-gradient-to-r from-[#FE5858] to-[#EE9617] text-slate-200 shadow-lg rounded-lg"
-		><a href="/register">Register</a></button
-	>
+		<p>Lets play some rock paper scissors!</p>
+		<div class="flex bg-red-400">
+			<button
+				class="font-serif p-4 font-medium bg-gradient-to-r from-[#0BAB64] to-[#3BB78F] text-slate-200 shadow-lg rounded-lg"
+				><a href="/login">Login</a></button
+			>
+			<button
+				class="p-4 font-medium bg-gradient-to-r from-[#FE5858] to-[#EE9617] text-slate-200 shadow-lg rounded-lg"
+				><a href="/register">Register</a></button
+			>
+		</div>
+	</div>
 </div>
